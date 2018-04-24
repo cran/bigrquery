e7544d97493ba550c5ea52dfd0a0e981494e9761
Hadley Wickham <hadley@rstudio.com>
2018-04-24 16:41:28 +0000

    version 1.0.0

--- tests/testthat/test-upload.r
+++ tests/testthat/test-upload.r
@@ -1,18 +1,19 @@
-context("upload")
+context("job-upload.R")
 
 test_that("date/times can be round-tripped", {
-  skip_if_no_auth()
+  ds <- bq_test_dataset()
 
   df1 <- data.frame(
-    x = Sys.Date(),
-    y = Sys.time()
+    x = as.Date("2018-01-01"),
+    y = as.POSIXct(as.Date("2018-01-01"))
   )
+  attr(df1$y, "tzone") <- "UTC"
   df1$z <- as.POSIXlt(df1$y)
 
-  job <- insert_upload_job("bigrquery-examples", "test", "x", df1)
+  job <- insert_upload_job(ds$project, ds$dataset, "x", df1)
   wait_for(job)
-  on.exit(delete_table("bigrquery-examples", "test", "x"))
 
-  df2 <- list_tabledata("bigrquery-examples", "test", "x")
+  df2 <- list_tabledata(ds$project, ds$dataset, "x")
   expect_equal(df1, df2)
 })
+
